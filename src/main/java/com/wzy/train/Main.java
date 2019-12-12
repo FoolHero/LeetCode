17ba85d5611a32dac9ef2ec9e17eae821c00d504
Wu <zeyuwu@ctrip.com>
2019-12-12 17:53:47 +0800

    +

---
+++ src/main/java/com/wzy/train/Main.java
@@ -0,0 +1,13 @@
+package com.wzy.train;
+
+/**
+ * @Author: wzy
+ * @Description:
+ * @Date: 2019/12/12 10:20
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello leetcode");
+    }
+}
