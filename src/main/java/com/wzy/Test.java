75e8aea18913308eb122040454d3a052c5444954
wzy <wzycaihua@gmail.com>
2023-07-22 16:12:59 +0800

    test new computer

--- src/main/java/com/wzy/Test.java
+++ src/main/java/com/wzy/Test.java
@@ -17,36 +17,7 @@ import java.util.List;
 
 class Client {
     public static void main(String[] args) throws Exception {
-        //Java源代码
-        String sourceStr = "public class Hello{    public String sayHello (String name) {return \"Hello,\" + name + \"!\";}}";
-        //类名及文件名
-        String clsName = "Hello";
-        //方法名
-        String methodName = "sayHello";
-        //当前编译器
-        JavaCompiler cmp = ToolProvider.getSystemJavaCompiler();
-        //Java标准文件管理器
-        StandardJavaFileManager fm = cmp.getStandardFileManager(null, null, null);
-        //Java文件对象
-        JavaFileObject jfo = new StringJavaObject(clsName, sourceStr);
-        //编译参数，类似于javac <options>中的options
-        List<String> optionsList = new ArrayList<String>();
-        //编译文件的存放地方，注意：此处是为Eclipse工具特设的
-        optionsList.addAll(Arrays.asList("-d", "./bin"));
-        //要编译的单元
-        List<JavaFileObject> jfos = Arrays.asList(jfo);
-        //设置编译环境
-        JavaCompiler.CompilationTask task = cmp.getTask(null, fm, null, optionsList, null, jfos);
-        //编译成功
-        if (task.call()) {
-            //生成对象
-            Object obj = Class.forName(clsName).newInstance();
-            Class<? extends Object> cls = obj.getClass();
-            //调用sayHello方法
-            Method m = cls.getMethod(methodName, String.class);
-            String str = (String) m.invoke(obj, "Dynamic Compilation");
-            System.out.println(str);
-        }
+        System.out.println("hello new computer");
     }
 }
 
